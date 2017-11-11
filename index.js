aa703ab92172f3e0003017dd994d76e805e3a1c4
Beth Schofield <bethmschofield@gmail.com>
2017-11-11 16:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
     if (string.toUpperCase() === string) {
       return "YES INDEED!"
     } else if ("I love you, Grandma." === string) {
-      return "I love you, too"
+      return "I love you, too."
     } else {
       return "I can't hear you!"
     }
